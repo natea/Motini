09090a2fd81de5f6a1cd14d7e97e39054543a59d
MikePK <mike@grazr.com>
2010-04-08 15:59:20 -0400

    Added a Controller to show the XML form of the rules

--- Dispatcher.py
+++ Dispatcher.py
@@ -18,16 +18,16 @@ from webob import exc, Request, Response
 
 from StartController import StartController
 from MotiniController import MotiniController
+from RulesController import RulesController
 
 class Dispatcher():
     '''Dispatcher'''
     def __init__(self,app=None):
         self.start = StartController()
         self.motini = MotiniController()
+        self.rules = RulesController()
         self.app = app
 
-        
-    #@wsgify
     def __call__(self,environ,start_response):
         req = Request(environ)
         if req.path_info.startswith('/start'):
@@ -38,6 +38,8 @@ class Dispatcher():
             return self.motini.clip(environ,start_response)
         elif req.path_info.startswith('/motini/theme'):
             return self.motini.theme(environ,start_response)
+        elif req.path_info == '/rules.xml':
+            return self.rules.index(environ,start_response)
 
         #resp(environ, start_response)
         if self.app:
